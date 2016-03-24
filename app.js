8595415677ced2ba0de4dc777bf725b919d26373
zhanggengxin <847691625@qq.ccom>
2016-03-24 09:58:16 +0800

    复习最近学的一些东西

--- app.js
+++ app.js
@@ -65,7 +65,7 @@
     var b = new Singleton('xinxin');
     console.log(a === b);
 })();
-
+var emailRE = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 var todoList = new Vue({
     el : '#todoList',
     data : {
@@ -73,13 +73,34 @@ var todoList = new Vue({
         newUser : {
             name : '',
             email : ''
+        },
+        errors : [
+            'Name cannot be empty.',
+            'Please provide a valid email address.'
+        ]
+    },
+    computed : {
+        validation : function(){
+            return {
+                name : !!this.newUser.name.trim(),
+                email : emailRE.test(this.newUser.email)
+            }
+        },
+        isValid : function(){
+            var validation = this.validation;
+            return Object.keys(validation).every(function (key){
+                return validation[key];
+            });
         }
     },
     methods : {
         addUser : function() {
-            this.users.push({name : this.newUser.name , email : this.newUser.email});
-            this.newUser.name = '';
-            this.newUser.email = '';
+            if (this.isValid()) {
+                this.users.push({name : this.newUser.name , email : this.newUser.email});
+                for (var i in this.newUser) {
+                    this.newUser[i] = '';
+                }
+            }
         },
         remove : function(index) {
             this.users.splice(index, 1);
@@ -87,9 +108,47 @@ var todoList = new Vue({
     }
 });
 
+Vue.component('tableTemp', {
+    template : '#tableTemp',
+    props : {
+        columns : Array,
+        data : Array,
+        filterKey : String
+    },
+    data : function(){
+        var itemEl = '';
+        var itemList = {};
+
+        this.columns.forEach(function(key){
+            itemList[key] = 1;
+        });
+        return {
+            itemEl : itemEl,
+            itemList : itemList
+        }
+    },
+    methods : {
+        choose : function(key) {
+            this.itemEl = key;
+            this.itemList[key] *= -1;
+        }
+    }
+});
 
 
-
+var tableTmpe = new Vue({
+    el : '#demo',
+    data : {
+        searchQuery : '',
+        gridColumns : ['name', 'power'],
+        gridData : [
+            { name: 'Chuck Norris', power: 2000 },
+            { name: 'Bruce Lee', power: 9000 },
+            { name: 'Jackie Chan', power: 7000 },
+            { name: 'Jet Li', power: 8000 }
+        ]
+    }
+});
 
 
 
