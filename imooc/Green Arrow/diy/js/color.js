6ce7f11b5e8f1ed8803d38fae423f1216fc8d7ed
zhanggengxin <847691625@qq.com>
2016-05-27 14:39:12 +0800

    绿箭侠 最终版 不改了
    降低等级判断难度。

--- imooc/Green Arrow/diy/js/color.js
+++ imooc/Green Arrow/diy/js/color.js
@@ -48,7 +48,7 @@
             },
 
             getGameOverText: function(lv) {
-                var b = lv > 20 ? Math.ceil(lv - 20) / 10 : 0,
+                var b = lv > 20 ? Math.ceil(Math.parseInt(lv - 20) / 3) : 0,
                     c = this.lvText[b] || _.last(this.lvText),
                     d = c + 'lv' + lv + '<br>尝尝辣鸡的味道';
                 return { html: d };
--- imooc/Green Arrow/diy/js/green.js
+++ imooc/Green Arrow/diy/js/green.js
@@ -37,7 +37,7 @@
             },
 
             getGameOverText: function(lv) {
-                var b = lv > 20 ? Math.ceil(lv - 20) / 10 : 0,
+                var b = lv > 10 ? Math.ceil(Math.parseInt(lv - 10) / 3) : 0,
                     c = this.lvText[b] || _.last(this.lvText),
                     d = c + 'lv' + lv + '<br>尝尝辣鸡的味道';
                 return { html: d };
