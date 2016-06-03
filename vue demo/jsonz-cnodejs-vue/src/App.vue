6feb86c209457ed292e87dde0c467fbe63302d79
zhanggengxin <847691625@qq.com>
2016-06-03 16:53:04 +0800

    添加less编译模块

--- vue demo/jsonz-cnodejs-vue/src/App.vue
+++ vue demo/jsonz-cnodejs-vue/src/App.vue
@@ -2,10 +2,36 @@
 	<div id="app">
 		<c-header></c-header>
 		<router-view></router-view>
+		<ul>
+			<li>1</li>
+		</ul>
 	</div>
 </template>
 
 <script>
 import cHeader from './components/header';
-export default {}
+export default {
+	components : {
+		cHeader
+	}
+}
 </script>
+
+<style lang="less">
+	html,
+	#app {
+		height : 100%;
+	}
+
+	body {
+		height: 100%;
+		font: 16px 'Microsoft YaHei', '微软雅黑';
+		background-color: #E1E1E1;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+</style>
---
+++ vue demo/jsonz-cnodejs-vue/src/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+	<header>
+		<a v-link="{name : 'index'}" class="brand">
+			<img src="https://o4j806krb.qnssl.com/public/images/cnodejs_light.svg" alt="cnodejs-logo">
+		</a>
+	</header>
+</template>
+
+<script>
+	
+</script>
