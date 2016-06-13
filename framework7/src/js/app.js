ea387c2023e3112589facb00acf7c8be305d06d6
zhanggengxin <847691625@qq.com>
2016-06-13 23:40:07 +0800

    have bug

--- framework7/src/js/app.js
+++ framework7/src/js/app.js
@@ -1,11 +1,42 @@
 (function(){
-    //initialize app
-    var myApp = new Framework7();
-
     // if we need to use custom dom library,let's save it to $$ variable;
     var $$ = Framework7.$;
 
-    // add view
+    //judge device
+    var isAndroid = Framework7.prototype.device.android === true,
+        isIos = Framework7.prototype.device.ios === true;
+
+    //We also need to use this conditions in our Template7 templates, so we may assigin it to Template7 global context
+    Template7.global = {
+        android: isAndroid,
+        ios: isIos
+    };
+
+    // Dynamically add style
+    if (isAndroid) {
+        $$('head').append('<meta name="android">');
+    } else {
+        $$('head').append('<meta name="ios">');
+    }
+
+    // and the final part before app initialization,we need to change Througe type navbar layout to fixed in material theme for Android
+    if (isAndroid) {
+        // change class
+        $$('.view.navbar-through').removeClass('navbar-through').addClass('navbar-fixed');
+
+        // and move navbar init page
+        $$('.view .navbar').prependTo('.view .page');
+    }
+
+    //initialize app
+    var myApp = new Framework7({
+        // Enable material theme for Android device only
+        material: !!isAndroid,
+        // enable template7 pages
+        template7Pages: true
+    });
+
+    // init view
     var mainView = myApp.addView('.view-main', {
         // because we want to use dynamic navbar,we need to enable it for this view
         dynamicNavbar: true
@@ -25,6 +56,4 @@
     $$(document).on('pageInit', '.page[data-page="about"', function(e){
         myApp.alert('here comes about page too');
     });
-
-    
 })();
