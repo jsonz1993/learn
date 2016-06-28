4183e7276508d224ac821c8431fca0b1cfccc16a
zhanggengxin <847691625@qq.com>
2016-06-28 17:01:38 +0800

    copy paste History

--- backbone/backbone/jsonz.js
+++ backbone/backbone/jsonz.js
@@ -1111,6 +1111,452 @@
                 this.setElement(_.result(this, 'el'), false);
             }
         }
-    })
+    });
+
+    // Backbone.sync
+    // -------------
+
+    // Override this function to change the manner in which Backbone persists
+    // models to the server. You will be passed the type of request, and the
+    // model in question. By default, makes a RESTful Ajax request
+    // to the model's `url()`. Some possible customizations could be:
+    //
+    // * Use `setTimeout` to batch rapid-fire updates into a single request.
+    // * Send up the models as XML instead of JSON.
+    // * Persist models via WebSockets instead of Ajax.
+    //
+    // Turn on `Backbone.emulateHTTP` in order to send `PUT` and `DELETE` requests
+    // as `POST`, with a `_method` parameter containing the true HTTP method,
+    // as well as all requests with the body as `application/x-www-form-urlencoded`
+    // instead of `application/json` with the model in a param named `model`.
+    // Useful when interfacing with server-side languages like **PHP** that make
+    // it difficult to read the body of `PUT` requests.
+    Backbone.sync = function(method, model, options) {
+        var type = methodMap[method];
+
+        // Default options, unless specified.
+        _.defaults(options || (options = {}), {
+            emulateHTTP: Backbone.emulateHTTP,
+            emulateJSON: Backbone.emulateJSON
+        });
+
+        // Default JSON-request options.
+        var params = { type: type, dataType: 'json' };
+
+        //Ensure that we have a URL.
+        if (!options.url) {
+            params.url = _.result(model, 'url') || urlError();
+        }
+
+        // Ensure that we have the appropriate request data.
+        if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
+            params.contentType = 'application/json';
+            params.data = JSON.stringify(options.attrs || model.toJSON(options))
+        }
+
+        // For older servers, emulate JSON by encoding the request into an HTML-form.
+        if (options.emulateJSON) {
+            params.contentType = 'application/x-www-form-urlencoded';
+            params.data = params.data ? { model: params.data } : {};
+        }
+
+        // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+        // And an `X-HTTP-Method-Override` header.
+        if (options.emulateHTTP && (type === 'PUT' || type === 'DELETE' || type === 'PATCH')) {
+            params.type = 'POST';
+            if (options.emulateJSON) params.data._method = type;
+            var beforeSend = options.beforeSend;
+            options.beforeSend = function(xhr) {
+                xhr.setRequestHeader('X-HTTP-Method-Override', type);
+                if (beforeSend) return beforeSend.apply(this, arguments);
+            };
+        }
+
+        // Don't process data on a non-GET request.
+        if (params.type !== 'GET' && !options.emulateJSON) {
+            params.processData = false;
+        }
+
+        /// If we're sending a `PATCH` request, and we're in an old Internet Explorer
+        // that still has ActiveX enabled by default, override jQuery to use that
+        // for XHR instead. Remove this line when jQuery supports `PATCH` on IE8.
+        if (params.type === 'PATCH' && noXhrPatch) {
+            params.xhr = function() {
+                return new ActiveXObject('Microsoft.XMLHTTP');
+            };
+        }
+
+        // Make the request, allowing the user to override any Ajax options.
+        var xhr = options.xhr = Backbone.ajax(_.extend(params, options));
+        model.trigger('request', model, xhr, options);
+        return xhr;
+    };
+
+    var noXhrPatch =
+        typeof window !== 'undefined' && !!window.ActiveXObject &&
+        !(window.XMLHttpRequest && (new XMLHttpRequest).dispatchEvent);
+
+    // Map from CRUD to HTTP for our default `Backbone.sync` implementation.
+    var methodMap = {
+        'create': 'POST',
+        'update': 'PUT',
+        'patch': 'PATCH',
+        'delete': 'DELETE',
+        'read': 'GET'
+    }
+
+    // Set the default implementation of `Backbone.ajax` to proxy through to `$`.
+    // Override this if you'd like to use a different library.
+    Backbone.ajax = function() {
+        return Backbone.$.ajax.apply(Backbone.$, arguments);
+    };
+
+    // Backbone.Router
+    // --------------
+
+    // Routers map faux-URLs to actions, and fire events when routes are
+    // matched. Creating a new one sets its `routes` hash, if not set statically.
+    var Router = Backbone.Router = function(options) {
+        options || (options = {});
+        if (options.routes) this.routes = options.routes;
+        this._bindRoutes();
+        this.ininialize.apply(this, arguments);
+    }
+
+    // Cached regular expression for matching named param parts and splatted
+    // parts of route strings.
+    var optionalParam = /\((.*?)\)/g;
+    var namedParam = /(\(\?)?:\w+/g;
+    var splatParam = /\*\w+/g;
+    var escapeRegExp = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+
+    // Set up all inheritable **Backbone.Router** properties and methods.
+    _.extend(Router.prototype, Events, {
+        // Initialize is an empty function by default. Override it with your own
+        // initialization logic.
+        initialize: function() {},
+
+        // Manually bind a single named route to a callback. For example:
+        //
+        //     this.route('search/:query/p:num', 'search', function(query, num) {
+        //       ...
+        //     });
+        //
+        route: function(route, name, callback) {
+            if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+            if (_.isFunction(name)) {
+                callback = name;
+                name = '';
+            }
+            if (!callback) callback = this[name];
+            var router = this;
+            Backbone.history.route(route, function(fragment) {
+                var args = router._extractParameters(route, fragment);
+                if (router.execute(callback, args) !== false) {
+                    router.trigger.apply(router, ['route:' + name].concat(args));
+                    router.trigger('router', name, agrs);
+                    Backbone.history.trigger('route', router, name, args);
+                }
+            });
+            return this;
+        },
+
+        // Execute a route handler with the provided parameters.  This is an
+        // excellent place to do pre-route setup or post-route cleanup.
+        execute: function(callback, args) {
+            if (ccallback) callback.apply(this, args);
+        },
+
+        // Simple proxy to `Backbone.history` to save a fragment into the history.
+        navigate: function(fragment, options) {
+            Backbone.history.navigate(fragment, options);
+            return this;
+        },
+
+        // Bind all defined routes to `Backbone.history`. We have to reverse the
+        // order of the routes here to support behavior where the most general
+        // routes can be defined at the bottom of the route map.
+        _bindRoutes: function() {
+            if (!this.routes) return;
+            this.routes = _.result(this, 'routes');
+            var route, routes = _.keys(this.routes);
+            whild((route = routes.pop()) != null) {
+                this.route(route, this.routes[route]);
+            }
+        },
+
+        // Convert a route string into a regular expression, suitable for matching
+        // against the current location hash.
+        _routeToRegExp: function(route) {
+            route = route.replace(escapeRegExp, '\\$&')
+                .replace(optionalParam, '(?:$1)?')
+                .replace(namedParam, function(match, optional) {
+                    return optional ? match : '([^/?]+)';
+                })
+                .replace(splatParam, '([^?]*?)');
+            return new RegExp('^' + route + '(?:\\?([\\s\\S]*))?$');
+        },
+
+        // Given a route, and a URL fragment that it matches, return the array of
+        // extracted decoded parameters. Empty or unmatched parameters will be
+        // treated as `null` to normalize cross-browser behavior.
+        _extractParameters: function(route, fragment) {
+            var params = route.exec(fragment).slice(1);
+            return _.map(params, function(param, i) {
+                // Don't decode the search params.
+                if (i === params.length -1) return param || null;
+                return param ? decodeURIComponent(param) : null;
+            });
+        }
+    });
+
+
+    // Backbone.History
+    // ----------------
+
+    // Handles cross-browser history management, based on either
+    // [pushState](http://diveintohtml5.info/history.html) and real URLs, or
+    // [onhashchange](https://developer.mozilla.org/en-US/docs/DOM/window.onhashchange)
+    // and URL fragments. If the browser supports neither (old IE, natch),
+    // falls back to polling.
+    var History = Bacbone.History = function(){
+        this.handlers = [];
+        _.bindAll(this, 'checkUrl');
+
+        // Ensure that `History` can be used outside of the browser.
+        if (typeof window !== 'undefined') {
+            this.location = window.location;
+            this.history = window.history;
+        }
+    };
+
+    // Cached regex for stripping a leading hash/slash and trailing space.
+    var routeStripper = /^[#\/]|\s+$/g;
+
+    // Cached regex for stripping leading and trailing slashes.
+    var rootStripper = /^\/+|\/+$/g;
+
+    // Cached regex for detecting MSIE.
+    var isExplorer = /msie [\w.]+/;
+
+    // Cached regex for removing a trailing slash.
+    var trailingSlash = /\/$/;
+
+    // Cached regex for stripping urls of hash.
+    var pathStripper = /#.*$/;
+
+    // Has the history handling already been started?
+    History.started = false;
+
+    // Set up all inheritable **Backbone.History** properties and methods.
+    _.extend(History.prototype, Events, {
+
+      // The default interval to poll for hash changes, if necessary, is
+      // twenty times a second.
+      interval: 50,
+
+      // Are we at the app root?
+      atRoot: function() {
+        return this.location.pathname.replace(/[^\/]$/, '$&/') === this.root;
+      },
+
+      // Gets the true hash value. Cannot use location.hash directly due to bug
+      // in Firefox where location.hash will always be decoded.
+      getHash: function(window) {
+        var match = (window || this).location.href.match(/#(.*)$/);
+        return match ? match[1] : '';
+      },
+
+      // Get the cross-browser normalized URL fragment, either from the URL,
+      // the hash, or the override.
+      getFragment: function(fragment, forcePushState) {
+        if (fragment == null) {
+          if (this._hasPushState || !this._wantsHashChange || forcePushState) {
+            fragment = decodeURI(this.location.pathname + this.location.search);
+            var root = this.root.replace(trailingSlash, '');
+            if (!fragment.indexOf(root)) fragment = fragment.slice(root.length);
+          } else {
+            fragment = this.getHash();
+          }
+        }
+        return fragment.replace(routeStripper, '');
+      },
+
+      // Start the hash change handling, returning `true` if the current URL matches
+      // an existing route, and `false` otherwise.
+      start: function(options) {
+        if (History.started) throw new Error("Backbone.history has already been started");
+        History.started = true;
+
+        // Figure out the initial configuration. Do we need an iframe?
+        // Is pushState desired ... is it available?
+        this.options          = _.extend({root: '/'}, this.options, options);
+        this.root             = this.options.root;
+        this._wantsHashChange = this.options.hashChange !== false;
+        this._wantsPushState  = !!this.options.pushState;
+        this._hasPushState    = !!(this.options.pushState && this.history && this.history.pushState);
+        var fragment          = this.getFragment();
+        var docMode           = document.documentMode;
+        var oldIE             = (isExplorer.exec(navigator.userAgent.toLowerCase()) && (!docMode || docMode <= 7));
+
+        // Normalize root to always include a leading and trailing slash.
+        this.root = ('/' + this.root + '/').replace(rootStripper, '/');
+
+        if (oldIE && this._wantsHashChange) {
+          var frame = Backbone.$('<iframe src="javascript:0" tabindex="-1">');
+          this.iframe = frame.hide().appendTo('body')[0].contentWindow;
+          this.navigate(fragment);
+        }
+
+        // Depending on whether we're using pushState or hashes, and whether
+        // 'onhashchange' is supported, determine how we check the URL state.
+        if (this._hasPushState) {
+          Backbone.$(window).on('popstate', this.checkUrl);
+        } else if (this._wantsHashChange && ('onhashchange' in window) && !oldIE) {
+          Backbone.$(window).on('hashchange', this.checkUrl);
+        } else if (this._wantsHashChange) {
+          this._checkUrlInterval = setInterval(this.checkUrl, this.interval);
+        }
+
+        // Determine if we need to change the base url, for a pushState link
+        // opened by a non-pushState browser.
+        this.fragment = fragment;
+        var loc = this.location;
+
+        // Transition from hashChange to pushState or vice versa if both are
+        // requested.
+        if (this._wantsHashChange && this._wantsPushState) {
+
+          // If we've started off with a route from a `pushState`-enabled
+          // browser, but we're currently in a browser that doesn't support it...
+          if (!this._hasPushState && !this.atRoot()) {
+            this.fragment = this.getFragment(null, true);
+            this.location.replace(this.root + '#' + this.fragment);
+            // Return immediately as browser will do redirect to new url
+            return true;
+
+          // Or if we've started out with a hash-based route, but we're currently
+          // in a browser where it could be `pushState`-based instead...
+          } else if (this._hasPushState && this.atRoot() && loc.hash) {
+            this.fragment = this.getHash().replace(routeStripper, '');
+            this.history.replaceState({}, document.title, this.root + this.fragment);
+          }
+
+        }
+
+        if (!this.options.silent) return this.loadUrl();
+      },
+
+      // Disable Backbone.history, perhaps temporarily. Not useful in a real app,
+      // but possibly useful for unit testing Routers.
+      stop: function() {
+        Backbone.$(window).off('popstate', this.checkUrl).off('hashchange', this.checkUrl);
+        // Some environments will throw when clearing an undefined interval.
+        if (this._checkUrlInterval) clearInterval(this._checkUrlInterval);
+        History.started = false;
+      },
+
+      // Add a route to be tested when the fragment changes. Routes added later
+      // may override previous routes.
+      route: function(route, callback) {
+        this.handlers.unshift({route: route, callback: callback});
+      },
+
+      // Checks the current URL to see if it has changed, and if it has,
+      // calls `loadUrl`, normalizing across the hidden iframe.
+      checkUrl: function(e) {
+        var current = this.getFragment();
+        if (current === this.fragment && this.iframe) {
+          current = this.getFragment(this.getHash(this.iframe));
+        }
+        if (current === this.fragment) return false;
+        if (this.iframe) this.navigate(current);
+        this.loadUrl();
+      },
+
+      // Attempt to load the current URL fragment. If a route succeeds with a
+      // match, returns `true`. If no defined routes matches the fragment,
+      // returns `false`.
+      loadUrl: function(fragment) {
+        fragment = this.fragment = this.getFragment(fragment);
+        return _.any(this.handlers, function(handler) {
+          if (handler.route.test(fragment)) {
+            handler.callback(fragment);
+            return true;
+          }
+        });
+      },
+
+      // Save a fragment into the hash history, or replace the URL state if the
+      // 'replace' option is passed. You are responsible for properly URL-encoding
+      // the fragment in advance.
+      //
+      // The options object can contain `trigger: true` if you wish to have the
+      // route callback be fired (not usually desirable), or `replace: true`, if
+      // you wish to modify the current URL without adding an entry to the history.
+      navigate: function(fragment, options) {
+        if (!History.started) return false;
+        if (!options || options === true) options = {trigger: !!options};
+
+        var url = this.root + (fragment = this.getFragment(fragment || ''));
+
+        // Strip the hash for matching.
+        fragment = fragment.replace(pathStripper, '');
+
+        if (this.fragment === fragment) return;
+        this.fragment = fragment;
+
+        // Don't include a trailing slash on the root.
+        if (fragment === '' && url !== '/') url = url.slice(0, -1);
+
+        // If pushState is available, we use it to set the fragment as a real URL.
+        if (this._hasPushState) {
+          this.history[options.replace ? 'replaceState' : 'pushState']({}, document.title, url);
+
+        // If hash changes haven't been explicitly disabled, update the hash
+        // fragment to store history.
+        } else if (this._wantsHashChange) {
+          this._updateHash(this.location, fragment, options.replace);
+          if (this.iframe && (fragment !== this.getFragment(this.getHash(this.iframe)))) {
+            // Opening and closing the iframe tricks IE7 and earlier to push a
+            // history entry on hash-tag change.  When replace is true, we don't
+            // want this.
+            if(!options.replace) this.iframe.document.open().close();
+            this._updateHash(this.iframe.location, fragment, options.replace);
+          }
+
+        // If you've told us that you explicitly don't want fallback hashchange-
+        // based history, then `navigate` becomes a page refresh.
+        } else {
+          return this.location.assign(url);
+        }
+        if (options.trigger) return this.loadUrl(fragment);
+      },
+
+      // Update the hash location, either replacing the current entry, or adding
+      // a new one to the browser history.
+      _updateHash: function(location, fragment, replace) {
+        if (replace) {
+          var href = location.href.replace(/(javascript:|#).*$/, '');
+          location.replace(href + '#' + fragment);
+        } else {
+          // Some browsers require that `hash` contains a leading #.
+          location.hash = '#' + fragment;
+        }
+      }
+
+    });
+
+    // Create the default Backbone.history.
+    Backbone.history = new History;
+
+    // Helpers
+    // -------
+
+    // Helper function to correctly set up the prototype chain, for subclasses.
+    // Similar to `goog.inherits`, but uses a hash of prototype properties and
+    // class properties to be extended.
+
+
 
 });
