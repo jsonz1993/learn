2509b159b1bfa73da9e9977d92f2b50800d885ec
张欣欣 <847691625@qq.com>
2016-07-04 23:21:11 +0800

    基础目录

---
+++ backbone/backbone_application_program_dev/fifth/js/models/book.js
@@ -0,0 +1,15 @@
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+	defaults: {
+		title: '',
+		completed: false
+	},
+
+	toggle: function(){
+		this.save({
+			completed: !this.get('completed')
+		})
+	}
+});
+
