82d5b467e14bab585c7186eb734c5c5a505bb5e9
zhanggengxin <847691625@qq.com>
2016-07-09 12:31:46 +0800

    add

---
+++ finish/require+backbone/js/views/cartCollectionView.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Administrator on 2016/7/9.
+ */
+define(
+    ['lib/backbone',
+     'views/ItemCollectionView',
+     'collections/cart'
+    ], function (Backbone, ItemCollectionView, Cart){
+
+        return Backbone.View.extend({
+            el: 'body',
+            events: {
+                'submit #add': 'addItem',
+                'submit #filter': 'filterItems',
+                'click #clear-filter': 'clearFilter'
+            },
+
+            initialize: function (items){
+                var cartCollection = new Cart(items);
+                this.itemView = new ItemCollectionView(cartCollection);
+            },
+
+            addItem: function (e){
+                e.preventDefault();
+                this.itemView.addItem();
+            },
+
+            filterItems: function(e) {
+                e.preventDefault();
+                this.itemView.filterByPrice();
+            },
+            clearFilter: function (e){
+                e.preventDefault();
+                this.itemView.clearFilter();
+            }
+        });
+});
