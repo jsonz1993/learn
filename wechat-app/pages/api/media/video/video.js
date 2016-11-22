f048c827d48de867777ed80e0bf3a79aa01b9e43
Jsonz <jsonz.zhang@herely.us>
2016-11-23 00:28:56 +0800

    feat(wehat-app): 官网api看一遍

--- wechat-app/pages/api/media/video/video.js
+++ wechat-app/pages/api/media/video/video.js
@@ -1,15 +1,15 @@
-Page({
-    bindButtonTap: function() {
-        var that = this
-        wx.chooseVideo({
-            sourceType: ['album','camera'],
-            maxDuration: 60,
-            camera: ['front','back'],
-            success: function(res) {
-                that.setData({
-                    src: res.tempFilePath
-                })
-            }
-        })
-    }
-})
+// Page({
+//     bindButtonTap: function() {
+//         var that = this
+//         wx.chooseVideo({
+//             sourceType: ['album','camera'],
+//             maxDuration: 60,
+//             camera: ['front','back'],
+//             success: function(res) {
+//                 that.setData({
+//                     src: res.tempFilePath
+//                 })
+//             }
+//         })
+//     }
+// })
