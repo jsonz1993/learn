4111cc5dd96266bbd61ee113600bfe2830755719
Jsonz <jsonz.zhang@herely.us>
2016-12-08 11:07:25 +0800

    feat(nightmare):

--- nightmarejs/ruan/taobao.test.js
+++ nightmarejs/ruan/taobao.test.js
@@ -1,19 +1,26 @@
 var Nightmare = require('nightmare'),
-	nightmare = Nightmare({show: true});
+    nightmare = Nightmare({ show: true ,waitTimeout: 10000 });
+var expect = require('chai').expect;
 
-nightmare
-	.goto('https:www.taobao.com')
-	.type('#q', '电视机')
-	.click('form[action*="/search"] [type=submit]')
-	.wait('#spulist-grid')
-	.evaluate(function() {
-		return document.querySelector('#spulist-grid .grid-item .info-cont')
-		.textContent.trim();
-	})
-	.end()
-	.then(function(result) {
-		console.log(result);
-	})
-	.catch(function(error) {
-		console.error('Search failed: ', error);
-	})
+describe('测试', function() {
+    it('ceshi', function(done) {
+        nightmare
+            .goto('https:www.taobao.com')
+            .type('#q', '电视机')
+            .click('form[action*="/search"] [type=submit]')
+            .wait('#spulist-grid')
+            .evaluate(function() {
+                return document.querySelector('#spulist-grid .grid-item .info-cont')
+                    .textContent.trim();
+            })
+            .end()
+            .then(function(result) {
+                console.log(result);
+                done();
+            })
+            .catch(function(error) {
+                console.error('Search failed: ', error);
+                done();
+            });
+    });
+});
