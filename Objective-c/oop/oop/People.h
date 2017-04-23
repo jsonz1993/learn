427a80d566b9ae1f2849e7d636544800b7eb21f5
Jsonz <jsonz@qq.com>
2017-04-23 10:57:20 +0800

    feat(Objective-C): oop

---
+++ Objective-c/oop/oop/People.h
@@ -0,0 +1,13 @@
+//
+//  People.h
+//  oop
+//
+//  Created by Jsonz on 2017/4/23.
+//  Copyright © 2017年 Jsonz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface People : NSObject
+
+@end
---
+++ Objective-c/oop/oop/People.m
@@ -0,0 +1,13 @@
+//
+//  People.m
+//  oop
+//
+//  Created by Jsonz on 2017/4/23.
+//  Copyright © 2017年 Jsonz. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+
+@end
