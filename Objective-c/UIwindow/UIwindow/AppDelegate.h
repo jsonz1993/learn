c5689400760ecef1183b15e6f2e1a489a5235b02
Jsonz <jsonz@qq.com>
2017-04-27 23:44:32 +0800

    feat(objectiveC) UIwindow

---
+++ Objective-c/UIwindow/UIwindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIwindow
+//
+//  Created by Jsonz on 2017/4/27.
+//  Copyright © 2017年 Jsonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-c/UIwindow/UIwindow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIwindow
+//
+//  Created by Jsonz on 2017/4/27.
+//  Copyright © 2017年 Jsonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
