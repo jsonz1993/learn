9eb63c6eadec301dcad25bbc1b473443d89d43ef
Jsonz <jsonz@qq.com>
2017-04-29 10:51:22 +0800

    update

---
+++ Objective-c/UIImageView/UIImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImageView
+//
+//  Created by Jsonz on 2017/4/29.
+//  Copyright © 2017年 Jsonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-c/UIImageView/UIImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImageView
+//
+//  Created by Jsonz on 2017/4/29.
+//  Copyright © 2017年 Jsonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
